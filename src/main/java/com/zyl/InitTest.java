8e6df148024354764af126a62ebb61108e8dffee
zhangyanlong <zhangyanlong1213@126.com>
2020-05-28 20:52:07 +0800

    init

---
+++ src/main/java/com/zyl/InitTest.java
@@ -0,0 +1,21 @@
+package com.zyl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * package com.zyl
+ * User: zhangyanlong
+ * Email: zhangyanlong@jd.com
+ * Date: 2020/1/13
+ * Time: 17:11
+ * Description:
+ */
+public class InitTest {
+
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("services.xml", "daos.xml");
+
+    }
+}
---
+++ src/main/java/com/zyl/service/PetStoreServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zyl.service;
+
+/**
+ * package com.zyl.service
+ * User: zhangyanlong
+ * Email: zhangyanlong@jd.com
+ * Date: 2020/1/13
+ * Time: 17:14
+ * Description:
+ */
+public class PetStoreServiceImpl {
+}
